3b02d1bedcf34c48d5d28cfdd97c57974abb8db2
dken1 <164333934+dken1@users.noreply.github.com>
2024-12-21 15:30:18 +0300

    Add files via upload

---
+++ LR5/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace LR5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cbBurger = new System.Windows.Forms.ComboBox();
+            this.btAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(466, 290);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // cbBurger
+            // 
+            this.cbBurger.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbBurger.FormattingEnabled = true;
+            this.cbBurger.Items.AddRange(new object[] {
+            "Бургер стандартный",
+            "Бургер с беконом"});
+            this.cbBurger.Location = new System.Drawing.Point(12, 318);
+            this.cbBurger.Name = "cbBurger";
+            this.cbBurger.Size = new System.Drawing.Size(161, 21);
+            this.cbBurger.TabIndex = 1;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(403, 318);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 2;
+            this.btAdd.Text = "Добавить";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 349);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.cbBurger);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cbBurger;
+        private System.Windows.Forms.Button btAdd;
+    }
+}
+
---
+++ LR5/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LR5.DBContent;
+using LR5.BuilderBurger;
+using LR5.BuilderBurger.BuilderBurger;
+
+namespace LR5
+{
+    public partial class Form1 : Form
+    {
+        Model1 model = new Model1();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            BurgerBuilder burgerBuilder = new BurgerBuilder();
+            BurgerDirector burgerDirector = new BurgerDirector(burgerBuilder);
+
+            if (cbBurger.SelectedItem.ToString() == "Бургер стандартный")
+            {
+                burgerDirector.BuildDefault();
+            }
+            else
+            {
+                burgerDirector.BuildWithBacon();
+            }
+            try
+            {
+                model.Burgers.Add(burgerBuilder.GetBurgers());
+                model.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            LoadData();
+        }
+        private void LoadData()
+        {
+            cbBurger.SelectedIndex = 0;
+            dataGridView1.DataSource = model.Burgers.ToList();
+        }
+    }
+}
